fca9ce1bf9b6a09d7b92df057cee3f97df8152fa
devanshtivari <devansh80771@gmail.com>
2022-09-14 05:00:29 +0530

    added files

--- App.js
+++ App.js
@@ -1,18 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
-import { TouchableOpacity } from 'react-native-web';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+import { useState } from 'react';
+import SingleTodo from './components/singletodo'
 
 export default function App() {
+  const[todo , setTodo] = useState('');
+  const[todos , setTodos] = useState([]);
+  
+  function addItem(){
+    if(!todo) return
+
+    setTodos([...todos,{ id: Date.now() , text: todo}]);
+    setTodo("");
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.heading}>React Native Tutorials</Text>
+      <Text style={styles.heading}>Task to be Done</Text>
+      <View style={styles.inputcontainer}>
       <TextInput
-        placeholder='Enter a task' 
+        placeholder='Enter a task'
+        value={todo}
+        onChangeText={text => setTodo(text)} 
         style={styles.input}
       />
-      <TouchableOpacity>
-        <Text style={styles.button}>Add</Text>
+      <TouchableOpacity style={styles.inputButton}>
+        <Text style={styles.button} onPress={addItem}>Add</Text>
       </TouchableOpacity>
+      </View>
+      <View style={styles.items}>
+        <FlatList
+          data={todos}
+          renderItem={
+            ({item})=><SingleTodo todo={item} setTodo={setTodos}/>
+          }
+          keyExtractor={
+            (item)=>item.id.toString()
+          }
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -25,12 +58,17 @@ const styles = StyleSheet.create({
     backgroundColor: "black",
   },
   heading: {
-    marginVertical:10,
+    marginVertical: 10,
     fontSize: 23,
-    fontWeight: '700',
+    fontWeight: "700",
     color: "#BABABA",
     alignItems: "center",
     marginTop: 30,
+  },
+  inputcontainer: {
+    flexDirection: "row",
+    marginHorizontal: 10,
+    alignItems: "center",
   },
   input: {
     backgroundColor: "white",
@@ -38,6 +76,30 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     paddingLeft: 15,
     paddingRight: 15,
-    height: "30px",
+    paddingTop: 5,
+    paddingBottom: 5,
+    marginLeft: "5%",
+    shadowColor: "#8A8A8A",
+    elevation: 10
+  },
+  inputButton: {
+    borderRadius: 10,
+  },
+  button: {
+    color: "white",
+    backgroundColor: "blue",
+    position: "absolute",
+    left: -65,
+    top: -18,
+    paddingLeft: 19,
+    paddingRight: 19,
+    paddingTop: 7,
+    paddingBottom: 7,
+    borderRadius: 10,
+  },
+  items:{
+    marginTop: 60,
+    width: "100%",
+    borderRadius: 15
   }
 });
---
+++ components/singletodo.js
@@ -0,0 +1,80 @@
+import React,{useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+
+export default function SingleTodo({todo , setTodos , todos }){
+    const[edit,setEdit] = useState(false);
+    const[edittext,setEdittext] = useState(todo.text);
+    
+    const handleEdit = ()=> {
+      if(!edit)
+      setEdit(!edit);
+      else{
+        setEdit(!edit);
+        setTodos(todos.map((t)=>
+         t.id === 
+        ));
+      }
+    }
+    return (
+      <View style={styles.todo}>
+        {!edit ? <Text style={styles.todotext}>{todo.text}</Text> : <TextInput style={styles.todoedit} value={todo.text} onChangeText={(text) => setEdittext(text)}/>}
+        <TouchableOpacity>
+          <Feather
+            style={styles.todoactions}
+            name="edit"
+            size={23}
+            color="black"
+            onPress={handleEdit}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <AntDesign
+            style={styles.todoactions}
+            name="delete"
+            size={23}
+            color="black"
+          />
+        </TouchableOpacity>
+      </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    todo: {
+        flexDirection: 'row',
+        backgroundColor: 'linen',
+        marginHorizontal: 12,
+        elevation: 10,
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        shadowColor: 'white',
+        marginBottom: 10,
+        borderRadius: 15,
+    },
+    todotext:{
+        flex:1,
+        fontSize: 18,
+        paddingVertical: 3,
+        padddingHorizontal: 5,
+    },
+    todoactions: {
+        marginLeft: 15,
+        marginTop: 5
+    },
+    todoedit: {
+        flex: 1,
+        fontSize: 18,
+        paddingHorizontal: 5,
+        borderRadius: 5,
+        borderColor: "gray",
+        borderWidth: 1,
+    }
+})
