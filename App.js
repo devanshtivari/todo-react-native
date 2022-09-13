ab27983d7993eac30e796a9b1850612ec4ff9627
devanshtivari <devansh80771@gmail.com>
2022-09-13 08:33:06 +0530

    adding files to the system

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-web';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.heading}>React Native Tutorials</Text>
+      <TextInput
+        placeholder='Enter a task' 
+        style={styles.input}
+      />
+      <TouchableOpacity>
+        <Text style={styles.button}>Add</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +21,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    backgroundColor: "black",
   },
+  heading: {
+    marginVertical:10,
+    fontSize: 23,
+    fontWeight: '700',
+    color: "#BABABA",
+    alignItems: "center",
+    marginTop: 30,
+  },
+  input: {
+    backgroundColor: "white",
+    width: "90%",
+    borderRadius: 10,
+    paddingLeft: 15,
+    paddingRight: 15,
+    height: "30px",
+  }
 });
